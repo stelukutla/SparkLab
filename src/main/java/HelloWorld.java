61084c8d18bc5caecff8d934ca2cfe47424b2fbe
Neel <sankara.telukutla@gmail.com>
2016-10-18 07:59:34 -0700

    This is sample !!!

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Test Git Hub and other if any related to project configuration.
+ *
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("This is test");
+    }
+}
